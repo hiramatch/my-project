b791fa496963a8c139acaf4584d4fb782dbf179f
hiramatch <hiramatch@gmail.com>
2017-07-09 22:10:27 +0900

    リファクタリング

--- src/components/Scene1.vue
+++ src/components/Scene1.vue
@@ -9,90 +9,22 @@
 <script>
 //  インポートします。
 import Vue from 'vue'
+import {NickNameHeader, InputText, InputButton} from './forms/Headers.js'
 
 //  デフォルト値定義
 export default {
   data () {
     return {
-
     }
   }
 }
 
-//  データストア
-var PropertyStore = {
-  debug: true,
-  state: {
-    property: {
-      type: '1',
-      nickname: ''
-    }
-  },
-  validate () {
-    this.debug && console.log('setMessageAction triggered with', this.state.property.nickname)
-
-    console.log(this.state.property.nickname.length)
-    if (this.state.property.nickname.length <= 3) {
-      throw new Error('3文字以上入力してください')
-    }
-    return true
-  }
-}
-
-//  ニックネーム表示用のヘッダーコンポーネントを作成
-var NickNameHeader = Vue.extend({
-  name: 'nickname_header',
-  template: '<div class="nickname">{{ sharedState.state.property.nickname }}</div>',
-  data: function () {
-    return {
-      privateState: {},
-      sharedState: PropertyStore
-    }
-  }
-})
 //  コンポーネントを登録
 Vue.component('nickname-header', NickNameHeader)
-
-//  インプットフォーム用のコンポーネントを作成
-var InputText = Vue.extend({
-  name: 'nickname_input',
-  template: '<input class="input" v-model="sharedState.state.property.nickname" placeholder="NICKNAMEを入力してください。">',
-  data: function () {
-    return {
-      privateState: {},
-      sharedState: PropertyStore
-    }
-  }
-})
 
 //  コンポーネントを登録
 Vue.component('input-text', InputText)
 
-//  ボタン用のコンポーネントを作成
-var InputButton = Vue.extend({
-  name: 'input-button',
-  template: '<button class="button" v-on:click="validate">START</button>',
-  data: function () {
-    return {
-      privateState: {},
-      sharedState: PropertyStore
-    }
-  },
-  methods: {
-    validate: function (event) {
-      console.log(this.$el)
-      var valid = false
-      try {
-        valid = this.$data.sharedState.validate()
-        if (valid) {
-          this.$router.push('/scene2')
-        }
-      } catch (e) {
-        alert(e.message)
-      }
-    }
-  }
-})
 //  コンポーネントを登録
 Vue.component('input-button', InputButton)
 </script>
