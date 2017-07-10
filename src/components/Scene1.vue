f438c350f5fbf15181267128f19361eaf6296547
hiramatch <hiramatch@gmail.com>
2017-07-10 21:39:29 +0900

    単一ファイルコンポーネント

--- src/components/Scene1.vue
+++ src/components/Scene1.vue
@@ -9,7 +9,9 @@
 <script>
 //  インポートします。
 import Vue from 'vue'
-import {NickNameHeader, InputText, InputButton} from './forms/Headers.js'
+import InputButton from './forms/InputButton'
+import NickNameHeader from './forms/Header'
+import InputText from './forms/InputText'
 
 //  デフォルト値定義
 export default {
@@ -39,26 +41,6 @@ Vue.component('input-button', InputButton)
     margin: 0 auto;
     height:30pt;
   }
-}
-.nickname{
-  display: block;
-  height: 3rem;
-}
-.input{
-  width: 130pt;
-  height:30pt;
-}
-.button {
-  display: block;
-  position: relative;
-  margin: 0 auto;
-  width: 70pt;
-  background-color: white;
-  border: solid 1px silver;
-  border-radius: 0.5rem 0.5rem;
-  padding: 0.5rem 1.5rem;
-  margin-top: 1rem;
-  text-decoration: none;
 }
 
 </style>
--- src/components/Scene2.vue
+++ src/components/Scene2.vue
@@ -2,14 +2,13 @@
   <div class="scene2">
     <h2>シーン２だよ</h2>
     <nickname-header></nickname-header>
-
   </div>
 </template>
 
 <script>
 //  インポート
 import Vue from 'vue'
-import {NickNameHeader} from './forms/Headers.js'
+import {NickNameHeader} from './forms/Header'
 
 //  デフォルト値定義
 export default {
